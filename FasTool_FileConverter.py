618c07064e18369c82fc5a1563e3c1337fcfbf3c
hmito <hmito@outlook.com>
2015-02-01 14:13:00 +0900

    Fasta, Qual, FastQの一連のファイルの読み込み処理まで作成

--- FasTool_FileConverter.py
+++ FasTool_FileConverter.py
@@ -26,13 +26,28 @@ import sys
 
 #define object for fasta data
 class FastaData:
+	def __init__(self,name,data):
+		self.name=name
+		self.data=data
+class QualData:
+	def __init__(self,name,qual):
+		self.name=name
+		self.qual=qual
+class FastQData:
 	def __init__(self,name,data,qual):
 		self.name=name
 		self.data=data
 		self.qual=qual
 
+#define object for command
+class Command:
+	def __init__(self,val1,val2,isRe):
+		self.val1=val1
+		self.val2=val2
+		self.isRe=isRe
+
 #read fasta file from File object
-def read_fasta(File, IsQual=0):
+def readFasta(File, IsQual=0):
 	FastaList=[]
 
 	IsName=0
@@ -50,9 +65,9 @@ def read_fasta(File, IsQual=0):
 			#Add a previous data into the FastaList if we have
 			if IsName and len(Data)>0:	
 				if IsQual:	
-					FastaList.append(FastaData(Name,"",Data))
+					FastaList.append(FastaData(Name,Data))
 				else:
-					FastaList.append(FastaData(Name,Data,""))
+					FastaList.append(QualData(Name,Data))
 				#reset logs
 				Name=""
 				Data=""
@@ -77,8 +92,52 @@ def read_fasta(File, IsQual=0):
 	return FastaList
 
 #read fastaq file from File object
-def read_fastaq(file):
-	return []
+def readFastQ(File):
+	FastaQList=[]
+
+	Mode=0	#0:WaitName, 1:WaitData, 2:WaitQual
+	Name=""
+	Data=""
+	Qual=""
+
+	regexIsDataName=re.compile(r"^@(.+)$",re.I)
+	regexIsQualBegin=re.compile(r"^\+",re.I)
+
+	for Line in File:
+		#erase CR and/or LF
+		Line=Line.rstrip()
+
+		#check whether the line is for a name of fasta
+		if regexIsDataName.match(Line):
+			#Add a previous data into the FastaQList if we have
+			if Mode>0 and len(Data)>0:	
+				FastaQList.append(FastQData(Name,Data,Qual))
+				#reset logs
+				Name=""
+				Data=""
+				Qual=""
+				Mode=0
+
+			#Get name of the fasta data and check the legnth of the name
+			#	We will ignore this data if the name is empty
+			Name=Line[1:]
+			if len(Name)>0:
+				Mode=1
+		else:
+			if Mode==1 and regexIsQualBegin.match(Line):
+				Mode=2
+				continue
+			
+			if Mode==1:
+				Data+=Line
+			elif Mode==2:
+				Qual+=Line
+
+	#Add a previous data into the FastaQList if we have
+	if Mode>0 and len(Data)>0:	
+		FastaQList.append(FastaData(Name,Data,Qual))
+
+	return FastaQList
 
 #make regex for getting full file name from file path.
 regexFullFileName=re.compile(r"[^/\\]+?$",re.I)	
@@ -92,7 +151,8 @@ Args.pop(0)
 #DataBuffer
 fastaData=[]
 qualData=[]
-fastaqData=[]
+FastQData=[]
+commandData=[]
 
 #read all files in arguments
 for Arg in Args:
@@ -101,15 +161,20 @@ for Arg in Args:
 	#read FullFileName from Arg
 	MatchFullFileName=regexFullFileName.search(Arg)
 	if not MatchFullFileName:
-		print("\t***Error*** fail to find this file.")
-		continue
+		print("********* ERROR *********\n> fail to find this file.")
+		print("\ntype any key...")
+		input()
+		sys.exit()
 	
 	
 	#separate FileName from file type name
 	MatchFileName=regexSeparateFileName.match(Arg,MatchFullFileName.start(),MatchFullFileName.end())
 	if not MatchFileName:
-		print("\t***Error*** fail to find file type.")
-		continue
+		print("********* ERROR *********\n> fail to find file type.")
+		print("\ntype any key...")
+		input()
+		sys.exit()
+
 	
 	#get filename and extension
 	FileName=MatchFileName.group(1)
@@ -121,20 +186,40 @@ for Arg in Args:
 
 	if FileType == "fasta" or FileType == "fas":
 		Fin=open(Arg,mode='r')
-		fastaData.append(read_fasta(Fin,0))
+		Data=readFasta(Fin,0)
+		print("\tNum: "+str(len(Data)))
+		fastaData.extend(Data)
 		Fin.close()
 
 	elif FileType == "qual":
 		Fin=open(Arg,mode='r')
-		qualData.append(read_fasta(Fin,1))
+		Data=readFasta(Fin,0)
+		print("\tNum: "+str(len(Data)))
+		qualData.extend(Data)
 		Fin.close()
 
-	elif FileType == "fastaq":
+	elif FileType == "fastq":
 		Fin=open(Arg,mode='r')
-		fastaqData.append(read_fastaq(Fin))
+		Data=readFastQ(Fin)
+		print("\tNum: "+str(len(Data)))
+		FastQData.extend(Data)
 		Fin.close()
 
-	for Datum in fastaData:
+	elif FileType == "txt":
+		Fin=open(Arg,mode='r')
+		Data=readFastQ(Fin)
+		print("\tNum: "+str(len(Data)))
+		FastQData.extend(Data)
+		Fin.close()
+	else:
+		print("********* ERROR *********\n> fail to detect file type.")
+		print("> Only fasta, fas, qual, fastq or txt files can be accepted.")
+		print("\ntype any key...")
+		input()
+		sys.exit()
+
+	for Datum in FastQData:
 		print(Datum.name)
 		print(Datum.data)
+		print(Datum.qual)
 		print("")
